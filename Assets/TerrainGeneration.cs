d840b11686db99d0c818547e46f3e3437484fc19
illjum7hae <124861446+illjum7hae@users.noreply.github.com>
2023-02-09 21:36:43 +0900

    내가 바꾼거

--- Assets/TerrainGeneration.cs
+++ Assets/TerrainGeneration.cs
@@ -7,37 +7,43 @@ using UnityEngine;
 
 public class TerrainGeneration : MonoBehaviour
 {
-    #region value
     public Sprite tile;
     public int worldSize = 100;
-    public float noiseFreq = 0.05f;
+   
+    public float heightMultiplier;
+
     public float seed;
+    #region seed
+    /*스트링 시드 = 시드(수정가능) + 이전 시드(수정불가)야
+     *acess는 에디터에서 접근하려고 만들었어
+     */
     private float saveSeed;
-    public float acessSaveSeed
-    { get { return saveSeed; } }
-
+    public float acessSaveSeed { get { return saveSeed; } }
     public float displaySavedSeed;
-    public Texture2D noiseTexture;
-    #endregion value
 
+    //현재 시드랑 노이즈를 바꿨을 떄의 시드를 따로 저장해
     private void Update()
-    {
-        displaySavedSeed = saveSeed;
-    }
+   =>   displaySavedSeed = saveSeed;
+
+    /*시드값 조절시 인스펙터 노이즈가 바뀐다*/
+    private void OnValidate()
+   =>   GenerateNoiseTexture();
+    #endregion seed
+    public float noiseFreq = 0.05f;
+    public Texture2D noiseTexture;
+
+   
     private void Start() {
         seed = Random.Range(-10000, 10000);
         GenerateNoiseTexture();
         GenerateTerrain();
-    }
-    private void OnValidate()
-    {
-        GenerateNoiseTexture();
     }
     
     private void GenerateTerrain()
     {
         for (int x = 0; x < worldSize; x++)
         {
+            float height = Mathf.PerlinNoise((x + seed) * noiseFreq, seed * noiseFreq) * heightMultiplier;
             for (int y = 0; y < worldSize; y++)
             {
                 if (noiseTexture.GetPixel(x, y).r < 0.5f)
@@ -46,7 +52,7 @@ public class TerrainGeneration : MonoBehaviour
                     newTile.transform.parent = this.transform;
                     newTile.AddComponent<SpriteRenderer>();
                     newTile.GetComponent<SpriteRenderer>().sprite = tile;
-                    newTile.transform.position = new Vector2(x, y);
+                    newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
                 }
             }
         }
