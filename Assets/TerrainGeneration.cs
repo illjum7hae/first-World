14c98729d2fc688cce0c9b8e81c90b859efded28
illjum7hae <124861446+illjum7hae@users.noreply.github.com>
2023-02-10 17:43:07 +0900

    지형 산 높이, 지형 시작 높이

--- Assets/TerrainGeneration.cs
+++ Assets/TerrainGeneration.cs
@@ -9,8 +9,11 @@ public class TerrainGeneration : MonoBehaviour
 {
     public Sprite tile;
     public int worldSize = 100;
-   
-    public float heightMultiplier;
+
+    public float caveFreq = 0.05f;
+    public float terrainFreq = 0.05f;
+    public float heightMultiplier = 4;
+    public float heightAddition = 25;
 
     public float seed;
     #region seed
@@ -27,12 +30,16 @@ public class TerrainGeneration : MonoBehaviour
 
     /*시드값 조절시 인스펙터 노이즈가 바뀐다*/
     private void OnValidate()
-   =>   GenerateNoiseTexture();
+    {
+        GenerateNoiseTexture();
+        GenerateTexture();
+    }
     #endregion seed
     public float noiseFreq = 0.05f;
     public Texture2D noiseTexture;
+    public Texture2D texture;
 
-   
+
     private void Start() {
         seed = Random.Range(-10000, 10000);
         GenerateNoiseTexture();
@@ -43,10 +50,11 @@ public class TerrainGeneration : MonoBehaviour
     {
         for (int x = 0; x < worldSize; x++)
         {
-            float height = Mathf.PerlinNoise((x + seed) * noiseFreq, seed * noiseFreq) * heightMultiplier;
-            for (int y = 0; y < worldSize; y++)
+            float height = Mathf.PerlinNoise((x + seed) * noiseFreq, seed * noiseFreq) * heightMultiplier + heightAddition;
+            for (int y = 0; y < height; y++)
             {
-                if (noiseTexture.GetPixel(x, y).r < 0.5f)
+                
+                //if (noiseTexture.GetPixel(x, y).r < 0.5f)
                 {
                     GameObject newTile = new GameObject(name = "tile");
                     newTile.transform.parent = this.transform;
@@ -65,10 +73,41 @@ public class TerrainGeneration : MonoBehaviour
         for (int x = 0; x < noiseTexture.width; x++) {
             for (int y = 0; y < noiseTexture.height; y++) {
                 float v = Mathf.PerlinNoise((x + seed) * noiseFreq, (y + seed) * noiseFreq);
+                #region what is perlinNoise
+                /* x = 1, y = 1, seed = -10000, noiseFreq = 0.05
+                 * (-9999 * 0.05, -9999 * 0.05)
+                 * (-499.95, -499.95)
+                 */
+                #endregion what is perlinNoise
                 noiseTexture.SetPixel(x, y, new Color(v,v,v));
             }
         }
         noiseTexture.Apply();
+    }
+    public int sizeTexture;
+    private void GenerateTexture()
+    {
+        texture = new Texture2D(sizeTexture, sizeTexture);
+        for (int count = 0; count < sizeTexture * sizeTexture; count++)
+        {
+            int x = count % sizeTexture;
+            int y = count / sizeTexture;
+
+            /*% and /
+             * print($"{count}%{sizeTexture}={x}, {count}/{sizeTexture}={y}");
+             * 01234 0 01234 1 01234 2 01234 3 01234 4 총 25번 출력, 이런 식이야
+            if (a >= 200) return;
+            */
+            float v = Mathf.PerlinNoise((x + seed) * noiseFreq, (y + seed) * noiseFreq);
+             #region what is perlinNoise
+             /* x = 1, y = 1, seed = -10000, noiseFreq = 0.05
+              * (-9999 * 0.05, -9999 * 0.05)
+              * (-499.95, -499.95)
+              */
+             #endregion what is perlinNoise
+             texture.SetPixel(x, y, new Color(v, v, v));
+        }
+        texture.Apply();
     }
 }
 
